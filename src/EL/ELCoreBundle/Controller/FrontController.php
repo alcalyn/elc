dc809a47ea97767e533c69196146734824965d4d
alcalyn <alcalyn@free.fr>
2013-11-01 20:15:53 +0100

    translation about

--- src/EL/ELCoreBundle/Controller/FrontController.php
+++ src/EL/ELCoreBundle/Controller/FrontController.php
@@ -14,11 +14,30 @@ class FrontController extends Controller
      * @Route(
      *      "/{_locale}",
      *      defaults = {"_locale": "en"},
-     *      name = "el_core_home"
+     *      name = "elcore_home"
      * )
      */
-    public function indexAction()
+    public function indexAction($_locale)
     {
-        return $this->render('ELCoreBundle:Front:index.html.twig');
+        $session = $this->get('session');
+        $session->start();
+        return $this->render('ELCoreBundle:Front:index.'.$_locale.'.html.twig');
+    }
+    
+    /**
+     * @Route(
+     *      "/en/about",
+     *      name = "elcore_en_about",
+     *      defaults = {"_locale": "en"}
+     * )
+     * @Route(
+     *      "/fr/a-propos",
+     *      name = "elcore_fr_about",
+     *      defaults = {"_locale": "fr"}
+     * )
+     */
+    public function aboutAction($_locale)
+    {
+        return $this->render('ELCoreBundle:Front:about.'.$_locale.'.html.twig');
     }
 }

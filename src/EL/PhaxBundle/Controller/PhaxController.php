1de37f4da2878133ae36f376bf9f9a1359fb0c52
alcalyn <alcalyn@free.fr>
2013-12-04 20:47:05 +0100

    trying...

--- src/EL/PhaxBundle/Controller/PhaxController.php
+++ src/EL/PhaxBundle/Controller/PhaxController.php
@@ -9,9 +9,35 @@ class PhaxController extends Controller
 {
     public function phaxAction($_locale)
     {
+        $request = $this->get('request');
+        
+        $params = null;
+        
+        if ($request->isMethod('post')) {
+            $params = $request->request->all();
+        } else {
+            $params = $request->query->all();
+        }
+        
+        $params['_locale'] = $_locale;
+        
+        $controller = $params['phax_controller'];
+        $action     = $params['phax_action'];
+        
+        
+        $sf_controller = implode(':', array(
+            'ELCoreBundle',
+            $controller,
+            $action,
+        ));
+        
         return new JsonResponse(array(
             'ok'        => 'ouais',
             'locale'    => $_locale,
+            'method'    => $request->getMethod(),
+            'params'    => $params,
+            'sf_ctrl'   => $sf_controller,
+            'result'    => $this->forward($sf_controller, $params),
         ));
     }
 }

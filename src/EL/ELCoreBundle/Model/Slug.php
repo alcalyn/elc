a57aeae60e2c7d9e6276a65ee7ab1289cbae9cd5
alcalyn <doubjulien@hotmail.fr>
2013-12-23 16:42:48 +0100

    Slug function

--- src/EL/ELCoreBundle/Model/Slug.php
+++ src/EL/ELCoreBundle/Model/Slug.php
@@ -7,14 +7,32 @@ class Slug
 {
     public static function slug($s, $prefix = '', $sufix = '')
     {
-        $r = $s;
+    	$r = $s;
+    	
+        // replace non letter or digits by -
+		$r = preg_replace('~[^\\pL\d]+~u', '-', $r);
+
+		// trim
+		$r = trim($r, '-');
+		
+		// transliterate
+		setlocale(LC_CTYPE, 'en_US');
+		$r = iconv('utf-8', 'ASCII//TRANSLIT', $r);
+		
+		// lowercase
+		$r = strtolower($r);
+		
+		// remove unwanted characters
+		$r = preg_replace('~[^-\w]+~', '', $r);
+		
+		if (empty($r)) {
+			$r = 'n-a-'.substr(uniqid('', true), -8);
+		}
         
-        $r = strtolower($r);
-        $r = str_replace(array(','), '', $r);
-        $r = preg_replace("/\s+/", ' ', $r);
-        $r = trim($r);
-        $r = str_replace(' ', '-', $r);
-        
+        // return
         return $prefix.$r.$sufix;
     }
+    
+    
+    
 }

17ba777956226e79efda1b65f4ece6108cc30e74
Alcalyn <doubjulien@hotmail.fr>
2014-04-11 15:51:12 +0200

    awale unserialize and serialize grid, core:Party nbplayer min and max integer to smallint

--- src/EL/AwaleBundle/Services/AwaleCore.php
+++ src/EL/AwaleBundle/Services/AwaleCore.php
@@ -25,4 +25,58 @@ class AwaleCore
         
         return $row.'|'.$row;
     }
+    
+    /**
+     * Return an array representing the grid such as:
+     * 
+     *  Array(
+     *      [0] => Array(
+     *          [seeds] => Array(
+     *              [0] => 2
+     *              [1] => 6
+     *              ...
+     *              [5] => 3
+     *          )
+     *          [attic] => 12
+     *      )
+     *      [1] => Array(
+     *          ...
+     *      )
+     *  )
+     * 
+     * @param string $grid
+     * 
+     * @return array
+     */
+    public function unserializeGrid($grid)
+    {
+        $rows = explode('|', $grid);
+        
+        $row0 = explode(';', $rows[0]);
+        $row1 = explode(';', $rows[1]);
+        
+        return array(
+            array(
+                'seeds' => explode(',', $row0[0]),
+                'attic' => $row0[1],
+            ),
+            array(
+                'seeds' => explode(',', $row1[0]),
+                'attic' => $row1[1],
+            ),
+        );
+    }
+    
+    /**
+     * @param array $grid
+     * 
+     * @return string
+     */
+    public function serializeGrid(array $grid)
+    {
+        $row0 = implode(',', $grid[0]['seeds']) . ';' . $grid[0]['attic'] ;
+        $row1 = implode(',', $grid[1]['seeds']) . ';' . $grid[1]['attic'] ;
+        
+        return $row0.'|'.$row1;
+    }
 }
